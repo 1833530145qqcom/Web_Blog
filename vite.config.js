8bb53bbf4d8d66926b0b59b7cb104e8b2ba91c7d
1833530145qqcom <115228141+1833530145qqcom@users.noreply.github.com>
2023-02-14 18:14:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/Web_Blog',
+  base:'./',
   build: {
     chunkSizeWarningLimit:1500,
     rollupOptions: {
