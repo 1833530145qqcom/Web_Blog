345b1abfe6f27a8779b004613e3beddc8b4157cb
1833530145qqcom <1833530145@qq.com>
2023-02-14 18:05:02 +0800

    2.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'/',
+  base:'/Web_Blog',
+  build: {
+    chunkSizeWarningLimit:1500,
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                
+                  return id.toString().split('node_modules/')[1].split('/')[0].toString();
+              }
+          }
+        }
+    }
+  },
   server: {
     hmr: true,
     port: 3001,
