548e2806881645d332c55f550a85f14a412ef13d
1833530145qqcom <115228141+1833530145qqcom@users.noreply.github.com>
2023-02-22 19:17:32 +0800

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,7 +7,7 @@ const user = ref('')
 const name = ref('')
 const password = ref('')
 const repeat = ref('')
-const host = 'https://db-api.amarea.cn/users'
+const host = 'https://db-api.amarea.cn'
 
 function login() {
   const myHeaders = new Headers()
