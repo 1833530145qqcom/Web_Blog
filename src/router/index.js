27f236a379aa4531aa10ee286af050dcf5b2f5b9
1833530145qqcom <115228141+1833530145qqcom@users.noreply.github.com>
2023-02-22 17:35:34 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,7 @@ const routes = [
 //这里不是通过new来创建，通过createRouter 方法创建，使用的模式不是通过mode来定义
 const router = createRouter({
     routes,
-    history: createWebHistory(),
+    history: createWebHashHistory(),
 })
 
 router.beforeEach((to, from, next) => {
