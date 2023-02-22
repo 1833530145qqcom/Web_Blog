7c8df006a7a0000767ed8acbd519379b0f94d03a
1833530145qqcom <115228141+1833530145qqcom@users.noreply.github.com>
2023-02-22 17:48:59 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import VueCookies from 'vue-cookies'
 const routes = [
     {
@@ -51,7 +51,7 @@ const routes = [
 //这里不是通过new来创建，通过createRouter 方法创建，使用的模式不是通过mode来定义
 const router = createRouter({
     routes,
-    history: createWebHashHistory(),
+    history: createWebHistory(),
 })
 
 router.beforeEach((to, from, next) => {
