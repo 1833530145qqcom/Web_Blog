dfb23d1581b8444fb7b7841edff795eee1adf1df
1833530145qqcom <115228141+1833530145qqcom@users.noreply.github.com>
2023-02-22 17:21:36 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,20 +18,7 @@ export default defineConfig({
         }
     }
   },
-  server: {
-    hmr: true,
-    port: 3001,
-    proxy: {
-      '/api': {
-        target: "http://localhost:8081/", // 目标代理接口地址
-        secure: false,
-        changeOrigin: true, // 开启代理，在本地创建一个虚拟服务端
-        pathRewrite: {
-          '^/api': '/api',
-        },
-      },
-    },
-  },
+
   resolve: {
     // 配置路径别名
     alias: {
